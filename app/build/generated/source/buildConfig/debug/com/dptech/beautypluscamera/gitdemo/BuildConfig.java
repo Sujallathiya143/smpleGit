a60e67e369664163ef83da545dd71afd8a928f72
Sujal PC <sujallathiyayami@gmail.com>
2022-11-26 11:24:59 +0530

    testing

---
+++ app/build/generated/source/buildConfig/debug/com/dptech/beautypluscamera/gitdemo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dptech.beautypluscamera.gitdemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dptech.beautypluscamera.gitdemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ mylibrary/src/main/java/com/dptech/beautypluscamera/mylibrary/testing.java
@@ -0,0 +1,10 @@
+package com.dptech.beautypluscamera.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class testing {
+    public void toast(Context context, String s) {
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
+}
